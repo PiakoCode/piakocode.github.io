3feb910afc4e3c22c47210ccb90cb90840564653
Piako <1163312641@qq.com>
2023-09-28 16:15:03 +0800

    2023年 09月 28日 星期四 16:15:03 CST

--- upload.sh
+++ upload.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-BLUE='\033[1;34m' # 蓝色
+BLUE='\033[1;34m'  # 蓝色
 GREEN='\033[0;32m' # 绿色
 NC='\033[0m'       # 恢复默认颜色
 
@@ -9,12 +9,12 @@ cd /home/Piako/Documents/markdown笔记
 echo -e "\n${BLUE}--------Start--------\n${NC}"
 git status
 
-
 git add .
 echo -e "\n${GREEN}Add all complete.\n${NC}"
 
 LANG=zh_CN.UTF-8 git commit -m "$(date)"
 echo -e "${GREEN}Commit complete.\n${NC}"
 
-git push
+git push origin main
+git push github main
 echo -e "${GREEN}-----Push complete!-----\n${NC}"
